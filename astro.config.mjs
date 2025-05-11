bcc98d2d687ec86b6cb2ff0f11d8b2444e76c156
Stanislav Ognyanov <stanislav.ognqnov@gmail.com>
2025-05-11 13:11:11 +0300

    remove fontawesome, add iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            open: true,
-        },
-    },
+    integrations: [icon()],
 });
